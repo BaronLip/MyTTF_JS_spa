c1242cced24155340e42d5624ec779ab6e7d8cdb
BaronLip <baronlip@gmail.com>
2019-11-04 11:49:28 -0600

    Updated HTML structure
    Created general format for page.

--- MyTTF_js_frontend/src/index.js
+++ MyTTF_js_frontend/src/index.js
@@ -1,1 +1,25 @@
-  const app = new App()
+document.addEventListener("DOMContentLoaded", launchApp)
+
+function launchApp() {
+        
+    
+    fetch
+}
+
+// document.getElementById("new-match-form").addEventListener("submit", newMatch)
+
+
+// function newMatch(e) {
+//     e.preventDefault()
+//     // console.log("hello")
+//     fetch("localhost:3000/api/matches", {
+//         method: "post",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//             name: e.match.value
+//         })
+
+//     })
+// }
---
+++ MyTTF_rails_backend/app/controllers/api/matches_controller.rb
@@ -0,0 +1,34 @@
+class MatchesController < ApplicationController
+
+    def index
+        matches = Match.all
+
+        render json: matches, status: 200
+    end
+
+    def show
+        match = Match.find_by(:id => params[:id])
+    
+        render json: match, status: 200
+    end
+
+    def create
+        match = Match.create(match_params)
+
+        render json: match, status: 200
+    end
+
+    def destroy
+        match = Match.find_by(:id => params[:id])
+        match.destroy
+
+        render json: {matchId: match.id}
+    end
+
+    private
+
+    def match_params
+        params.require(:match).permit(:body)
+    end
+
+end
